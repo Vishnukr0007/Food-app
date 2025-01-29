21fec1b396f60d18c46ce2764ba75d93743feb1e
Vishnukr0007 <vishnukrishnankutty54@gmail.com>
2025-01-30 00:15:09 +0530

    first commit

---
+++ Server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const menuRoutes = require("./routes/MenuRoutes")
+const menuItemsRoutes = require('./routes/MenulistRoutes');
+const itemRoutes=require('./routes/ItemsRoute')
+require('dotenv').config();
+const app = express();
+const PORT = process.env.port ||  3000;
+//middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+app.use("/api", menuRoutes);
+app.use('/api/menu', menuItemsRoutes);
+app.use('/api',itemRoutes)
+
+mongoose.connect(process.env.MONGO_URI, {
+  
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log('Error connecting to MongoDB:', err));
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
