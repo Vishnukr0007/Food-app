9f5adef4a6bd89f5e9a071a708534ba652ce2149
Vishnukr0007 <116253973+Vishnukr0007@users.noreply.github.com>
2025-01-30 00:28:30 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -7,7 +7,7 @@ const menuItemsRoutes = require('./routes/MenulistRoutes');
 const itemRoutes=require('./routes/ItemsRoute')
 require('dotenv').config();
 const app = express();
-const PORT = process.env.port ||  3000;
+const PORT = process.env.port ||  5000;
 //middleware
 app.use(cors());
 app.use(bodyParser.json());
